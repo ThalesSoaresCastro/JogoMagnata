9c083418c2e88bcf134a8f0effbb4b63e82d59e0
Thales <th4les.castro@gmail.com>
2018-10-28 23:37:34 -0300

    Atualização Animação Moeda e Vida

--- renome.py
+++ renome.py
@@ -1,9 +1,9 @@
 import os
 num=0
 
-for nome in os.listdir('./explosao'):
+for nome in os.listdir('./health'):
 	print(str(nome))
 	new=str(num)+".png"
-	os.rename("./explosao/"+str(nome), "./explosao/"+str(new))
+	os.rename("./health/"+str(nome), "./health/"+str(new))
 	print("antigo: "+ str(nome) + "novo: "+ str(new))
 	num+=1
